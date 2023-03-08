2724639e2485ce3d2f406955f6d6e2783292994f
Kuszki <l.drozdz@o2.pl>
2023-03-08 03:19:56 +0100

    Add round error shapes

--- get_shapefact.m
+++ get_shapefact.m
@@ -3,8 +3,8 @@ function [s] = get_shapefact(c1 = 'a', c2 = 'a', check = true)
   all = strcmp(c1, 'a') && strcmp(c2, 'a');
 
   if check && !all
-    assert(length(c1) == 1 && sum(c1 == 'nuts') == 1, 'shape must be "u", "n", "t" or "s"')
-    assert(length(c2) == 1 && sum(c2 == 'nuts') == 1, 'shape must be "u", "n", "t" or "s"')
+    assert(length(c1) == 1 && sum(c1 == 'nutsr') == 1, 'shape must be "u", "n", "t", "s" or "r"')
+    assert(length(c2) == 1 && sum(c2 == 'nutsr') == 1, 'shape must be "u", "n", "t", "s" or "r"')
   end
 
   m = containers.Map();
@@ -14,15 +14,21 @@ function [s] = get_shapefact(c1 = 'a', c2 = 'a', check = true)
   m("nn") = 0.00000;
   m("ss") = 0.71435;
   m("tt") = 0.04151;
+  m("rr") = 0.02726;
 
   m("un") = m("nu") = 0.15613;
   m("us") = m("su") = 0.53369;
   m("ut") = m("tu") = 0.17734;
+  m("ur") = m("ru") = 0.06612;
 
   m("ns") = m("sn") = 0.29881;
   m("nt") = m("tn") = 0.02499;
+  m("nr") = m("rn") = -0.0091;
 
   m("st") = m("ts") = 0.35037;
+  m("sr") = m("rs") = 0.19715;
+
+  m("tr") = m("rt") = -0.0101;
 
   if all
     s = m;
