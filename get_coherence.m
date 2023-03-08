bee3e7e7c3f30fa7ac9f5c38352c98c16823e3d0
Kuszki <l.drozdz@o2.pl>
2023-03-08 12:50:17 +0100

    Can be finall

--- get_coherence.m
+++ get_coherence.m
@@ -1,9 +1,12 @@
-function [h, s, k1, k2] = get_coherence(c1, c2, r12 = 0.0, n1 = 0, n2 = 0, vect = [], r = 0, check = true)
+function [h, s, k1, k2] = get_coherence(c1, c2, r12 = 0.0, n1 = 0, n2 = 0, uv = [], rv = [], check = true)
+
+  isrok = !check || !length(rv) || (rows(rv) == length(uv) && columns(rv) == length(uv));
 
   if check
     assert(n1 + n2 == 0 || n1 + n2 >= 2, 'all or noone indexes must be zero given');
-    assert(n1 >= 0 && n1 <= length(vect), 'index n1 must be in range of uncertainty vector');
-    assert(n2 >= 0 && n2 <= length(vect), 'index n2 must be in range of uncertainty vector');
+    assert(n1 >= 0 && n1 <= length(uv), 'index n1 must be in range of uncertainty uvor');
+    assert(n2 >= 0 && n2 <= length(uv), 'index n2 must be in range of uncertainty uvor');
+    assert(isrok, 'rv must be square matrix with length(uv) x length(uv) dimmension');
   end
 
   if n1 == n2 && n1 != 0; r = s = k1 = k2 = 1.0;
@@ -11,24 +14,28 @@ function [h, s, k1, k2] = get_coherence(c1, c2, r12 = 0.0, n1 = 0, n2 = 0, vect 
   else
 
     s = get_shapefact(c1, c2);
+    k1 = k2 = 1.0;
 
-    if length(vect)
+    if length(uv)
 
-      if length(r) == 1; r = diag(ones(1, length(c))); end;
+      if !length(rv); rv = diag(ones(1, length(c))); end;
 
-      us = vect * r * transpose(vect);
-      ss = sum(vect .^ 2);
-      d1 = sqrt(us) - sqrt(ss)
+      u1 = uv(n1); u2 = uv(n2);
+
+      us = uv * rv * transpose(uv);
+      ss = sum(uv .^ 2);
+
+      d1 = sqrt(us) - sqrt(ss);
       d2 = abs(d1);
 
-      k1 = (vect(n1)^2 + vect(n2)^2 + d1^2) / ss
+      k1 = u1^2 + u2^2 + d1^2;
+      k1 = k1 / ss;
 
-      if d1 < 0; k1 = 1-k1; end;
-
-      k2 = (min(vect(n1)+ d2, vect(n2)+ d2)) / (max(vect(n1)+ d2, vect(n2)+ d2));
+      k2 = (min(u1, u2) + d2) / max(u1, u2);
       k2 = k2^(1/3);
-    else
-      k1 = k2 = 1.0;
+
+      if d1 < 0; k1 = 1 - k1; end;
+
     end
 
     h = s * abs(k1) * abs(k2);
