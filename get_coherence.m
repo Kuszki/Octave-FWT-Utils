08d456c85020a513295eee8d93d100769a42b133
Kuszki <l.drozdz@o2.pl>
2023-03-09 16:26:42 +0100

    Implement cohers from datafile

--- get_coherence.m
+++ get_coherence.m
@@ -1,4 +1,4 @@
-function [h, s, k1, k2] = get_coherence(c1, c2, r12 = 0.0, n1 = 0, n2 = 0, uv = [], rv = [], check = true)
+function [h, s, k1, k2] = get_coherence(c1, c2, r12 = 0.0, n1 = 0, n2 = 0, uv = [], rv = [], check = false)
 
   isrok = !check || !length(rv) || (rows(rv) == length(uv) && columns(rv) == length(uv));
 
@@ -10,7 +10,17 @@ function [h, s, k1, k2] = get_coherence(c1, c2, r12 = 0.0, n1 = 0, n2 = 0, uv = 
   end
 
   if n1 == n2 && n1 != 0; r = s = k1 = k2 = 1.0;
-  elseif r12 != 0.0; h = s = r12; k1 = k2 = 1.0;
+  elseif r12 != 0.0
+
+     cp = sprintf("%s_%s", c1, c2);
+     dat = load('dat_coherences.gz').(cp);
+
+     s = interp1(dat(:,1), dat(:,2), 0.0);
+     h = interp1(dat(:,1), dat(:,2), r12);
+
+     k1 = k2 = 1.0;
+     #h = s = r12;
+
   else
 
     s = get_shapefact(c1, c2);
@@ -31,14 +41,18 @@ function [h, s, k1, k2] = get_coherence(c1, c2, r12 = 0.0, n1 = 0, n2 = 0, uv = 
       k1 = u1^2 + u2^2 + d1^2;
       k1 = k1 / ss;
 
-      k2 = (min(u1, u2) + d2) / max(u1, u2);
-      k2 = k2^(1/3);
+      k2 = (min(u1, u2) + d2) / (max(u1, u2));
+      k2 = k2^(1/2);
 
       if d1 < 0; k1 = 1 - k1; end;
+
+      #assert(k1 >= 0 && k1 <= 1, sprintf('range k1 = %f', k1))
+      #assert(k2 >= 0 && k2 <= 1, sprintf('range k2 = %f', k2))
 
     end
 
     h = s * abs(k1) * abs(k2);
+    #assert(h >= -1 && h <= 1, sprintf('range h = %f', h))
 
   end
 
--- get_shapefact.m
+++ get_shapefact.m
@@ -1,39 +1,49 @@
-function [s] = get_shapefact(c1 = 'a', c2 = 'a', check = true)
+function [s] = get_shapefact(c1 = 'a', c2 = 'a', mode = 'table', check = false)
 
-  all = strcmp(c1, 'a') && strcmp(c2, 'a');
+  all = strcmp(c1, 'a') && strcmp(c2, 'a') && !strcmp('file');
 
   if check && !all
     assert(length(c1) == 1 && sum(c1 == 'nutsr') == 1, 'shape must be "u", "n", "t", "s" or "r"')
     assert(length(c2) == 1 && sum(c2 == 'nutsr') == 1, 'shape must be "u", "n", "t", "s" or "r"')
   end
 
-  m = containers.Map();
-  key = [c1 c2];
+  if strcmp(mode, 'file')
 
-  m("uu") = 0.33560;
-  m("nn") = 0.00000;
-  m("ss") = 0.71435;
-  m("tt") = 0.04151;
-  m("rr") = 0.02726;
+    cp = sprintf("%s_%s", c1, c2);
+    dat = load('dat_coherences.gz').(cp);
+    s = interp1(dat(:,1), dat(:,2), 0.0)
 
-  m("un") = m("nu") = 0.15613;
-  m("us") = m("su") = 0.53369;
-  m("ut") = m("tu") = 0.17734;
-  m("ur") = m("ru") = 0.06612;
-
-  m("ns") = m("sn") = 0.29881;
-  m("nt") = m("tn") = 0.02499;
-  m("nr") = m("rn") = -0.0091;
-
-  m("st") = m("ts") = 0.35037;
-  m("sr") = m("rs") = 0.19715;
-
-  m("tr") = m("rt") = -0.0101;
-
-  if all
-    s = m;
   else
-    s = m(key);
+
+    m = containers.Map();
+    key = [c1 c2];
+
+    m("uu") = 0.33560;
+    m("nn") = 0.00000;
+    m("ss") = 0.71435;
+    m("tt") = 0.04151;
+    m("rr") = 0.02726;
+
+    m("un") = m("nu") = 0.15613;
+    m("us") = m("su") = 0.53369;
+    m("ut") = m("tu") = 0.17734;
+    m("ur") = m("ru") = 0.06612;
+
+    m("ns") = m("sn") = 0.29881;
+    m("nt") = m("tn") = 0.02499;
+    m("nr") = m("rn") = -0.0091;
+
+    m("st") = m("ts") = 0.35037;
+    m("sr") = m("rs") = 0.19715;
+
+    m("tr") = m("rt") = -0.0101;
+
+    if all
+      s = m;
+    else
+      s = m(key);
+    end
+
   end
 
 end
