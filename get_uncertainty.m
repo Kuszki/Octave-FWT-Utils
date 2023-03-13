7b56b54d3e6b46d3b7323195eb357f2b97f50bfe
Kuszki <l.drozdz@o2.pl>
2023-03-13 08:43:31 +0100

    Minor fix

--- get_uncertainty.m
+++ get_uncertainty.m
@@ -1,28 +1,33 @@
-function [up, um, s, w, m] = get_uncertainty(y, alpha = 95, mode = 's', check = false)
+function [up, um, s, w, m] = get_uncertainty(y, alpha = 95, issym = true, check = false)
 
 	if check
 		assert(alpha > 0 && alpha < 100, 'alpha must be in range (0, 100)');
 		assert(isvector(y) && length(y) > 1, 'y must be a vector');
 	end
 
-	m = mean(y); w = var(y); s = sqrt(w);
-	[n, x] = hist(y, 1001, 100);
+	m = mean(y);
+	w = var(y);
+	s = sqrt(w);
 
-	ip = 1; ta = 0;
+	ln = 3000;
+	ip = 1;
+	ta = 0;
+
+	[n, x] = hist(y, ln, 100);
 
 	while x(ip) < m; ++ip; end
 
-	ta = n(im = ip);
+	im = ip--;
 
 	while ta < alpha
 		if im > 1; ta = ta + n(--im); end
-		if ip < 1001 ta = ta + n(++ip); end
+		if ip < ln ta = ta + n(++ip); end
 	end
 
 	up = x(ip);
 	um = x(im);
 
-	if strcmp(mode, 's')
+	if issym
 		up = (up - um) / 2.0;
 		um = up / s;
 	end
