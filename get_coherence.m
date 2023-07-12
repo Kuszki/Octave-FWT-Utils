d1f04dc3b53f009a78ad23c85f31eb0181a4b177
Kuszki <l.drozdz@o2.pl>
2023-07-12 23:06:28 +0200

    Minor

--- get_coherence.m
+++ get_coherence.m
@@ -2,14 +2,15 @@ function [h, s, k1, k2] = get_coherence(c1, c2, n1 = 0, n2 = 0, uv = [], check =
 
 	if check
 		assert(n1 + n2 == 0 || n1 + n2 >= 2, 'all or noone indexes must be zero given');
-		assert(n1 >= 0 && n1 <= length(uv), 'index n1 must be in range of uncertainty uvor');
-		assert(n2 >= 0 && n2 <= length(uv), 'index n2 must be in range of uncertainty uvor');
+		assert(n1 >= 0 && n1 <= length(uv), 'index n1 must be in range of uncertainty vector');
+		assert(n2 >= 0 && n2 <= length(uv), 'index n2 must be in range of uncertainty vector');
 	end
 
 	if n1 == n2 && n1 != 0; h = s = k1 = k2 = 1.0;
 	else
 
 		if !length(uv); k1 = k2 = 1.0;
+		elseif uv(n1) == 0.0 || uv(n2) == 0; k1 = k2 = 0.0;
 		else
 
 			u1 = uv(n1); u2 = uv(n2);
