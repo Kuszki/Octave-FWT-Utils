dd1ae1854925db21072be831a2b5defd1001f64a
Kuszki <l.drozdz@o2.pl>
2024-05-17 12:56:10 +0200

    Add confidence level values

--- gen_coherence.m
+++ gen_coherence.m
@@ -3,27 +3,8 @@ function [h, r] = gen_coherence(p1, p2, c1, c2, mode = 'u', alpha = 95, num = 1e
 	assert(sum(c1 == "nuts") == 1, 'c1 must be "n", "u", "t" or "s"');
 	assert(sum(c2 == "nuts") == 1, 'c2 must be "n", "u", "t" or "s"');
 
-	switch (c1)
-		case 'n'
-			x1 = gen_randn(num, p1, mode);
-		case 'u'
-			x1 = gen_randu(num, p1, mode);
-		case 's'
-			x1 = gen_rands(num, p1, mode);
-		case 't'
-			x1 = gen_randt(num, p1, mode);
-	end
-
-	switch (c2)
-		case 'n'
-			x2 = gen_randn(num, p2, mode);
-		case 'u'
-			x2 = gen_randu(num, p2, mode);
-		case 's'
-			x2 = gen_rands(num, p2, mode);
-		case 't'
-			x2 = gen_randt(num, p2, mode);
-	end
+	x1 = gen_randm(num, c1, p1, mode, alpha, false);
+	x2 = gen_randm(num, c2, p2, mode, alpha, false);
 
 	[h, r] = get_corelation(x1, x2, alpha, false);
 
--- gen_randm.m
+++ gen_randm.m
@@ -1,4 +1,4 @@
-function [r] = gen_randm(num, c, u = 1.0, mode = 'u', check = true)
+function [r] = gen_randm(num, c, u = 1.0, mode = 'u', alpha = 95, check = true)
 
 	if length(u) == 1
 		u = u * ones(1, length(c));
@@ -8,17 +8,27 @@ function [r] = gen_randm(num, c, u = 1.0, mode = 'u', check = true)
 		assert(length(c) == length(u), 'c and u must be the same length');
 	end
 
+	switch alpha
+		case 90; covs = [   1.645   1.559   1.675   1.397   ];
+		case 91; covs = [   1.695   1.576   1.715   1.400   ];
+		case 92; covs = [   1.751   1.593   1.757   1.403   ];
+		case 93; covs = [   1.812   1.611   1.801   1.405   ];
+		case 94; covs = [   1.881   1.628   1.849   1.408   ];
+		case 95; covs = [   1.960   1.645   1.902   1.409   ];
+		case 96; covs = [   2.054   1.663   1.960   1.411   ];
+		case 97; covs = [   2.170   1.680   2.025   1.412   ];
+		case 98; covs = [   2.326   1.697   2.103   1.413   ];
+		case 99; covs = [   2.576   1.715   2.205   1.414   ];
+		otherwise error('select alpha level in range 90-99%');
+	end
+
 	for i = 1 : length(c)
 
 		switch (c(i))
-			case 'u'
-				r(i,:) = gen_randu(num, u(i), mode);
-			case 'n'
-				r(i,:) = gen_randn(num, u(i), mode);
-			case 's'
-				r(i,:) = gen_rands(num, u(i), mode);
-			case 't'
-				r(i,:) = gen_randt(num, u(i), mode);
+			case 'n'; r(i,:) = gen_randn(num, u(i), mode, covs(1));
+			case 'u'; r(i,:) = gen_randu(num, u(i), mode, covs(2));
+			case 't'; r(i,:) = gen_randt(num, u(i), mode, covs(3));
+			case 's'; r(i,:) = gen_rands(num, u(i), mode, covs(4));
 		end
 
 	end
